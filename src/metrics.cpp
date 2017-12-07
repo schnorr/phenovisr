12c3f58ff74a3d5c932f1014af5a982260fba5c4
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-12-07 18:45:54 -0200

    H implementation

--- src/metrics.cpp
+++ src/metrics.cpp
@@ -15,12 +15,18 @@ static float get_blue_average (unsigned char r, unsigned char g, unsigned char b
   return (r+g+b) == 0 ? 0 : (float)b/(float)(r+g+b);
 }
 
+static float get_H_average (unsigned char r, unsigned char g, unsigned char b)
+{
+  return 0;
+}
+
 float get_average (PGAMetricType type, unsigned char r, unsigned char g, unsigned char b)
 {
   switch (type){
   case Red: return get_red_average (r, g, b);
   case Green: return get_green_average (r, g, b);
   case Blue: return get_blue_average (r, g, b);
+  case H: return get_H_average (r, g, b);
   case Undef:
   default: return get_green_average (r, g, b);
   }
