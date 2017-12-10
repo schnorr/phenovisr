940a7bab92d2d3de1b32d531efbab0939986e454
Lucas M. Schnorr <schnorr@inf.ufrgs.br>
2017-12-10 09:39:05 -0200

    new struct to keep double histogram of HSV counts

--- src/metrics.h
+++ src/metrics.h
@@ -3,6 +3,13 @@
 #include "jpeg_image.h"
 #include "rgb2hsv.h"
 
+typedef struct hsv_histogram {
+  int H;       // The H coordinate
+  int count;   // The count of the given H
+  int nsubins; // How many subins of V?
+  int *V;      // A vector of nsubins with the counts of V
+}hsv_histogram_t;
+
 double get_metric (PGAMetricType type,
 		   unsigned char r,
 		   unsigned char g,
